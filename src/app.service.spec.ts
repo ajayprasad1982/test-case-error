392b992428463ef28577768643f08ce64fa37444
Ajay <ajay.ctc@gmail.com>
2021-02-06 16:27:53 +0530

    Add files via upload
    modified

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,12 +1,26 @@
 import { AppService } from './app.service';
 import { Test } from '@nestjs/testing';
+import { getLoggerToken } from './logger/logger.utils';
+import { HttpService } from '@nestjs/common';
 
 describe('AppService', () => {
   let service: AppService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: getLoggerToken('AppService'),
+          useValue: {
+          },
+        },
+        {
+          provide: HttpService,
+          useValue: {
+          },
+        }
+      ]
     }).compile();
 
     service = module.get<AppService>(AppService);
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 import { Logger } from './logger/logger.decorator';
 import { LoggerSrv } from './logger/logger.service';
 @Injectable()
 export class AppService {
-  constructor(@Logger('AppService') private logger: LoggerSrv) {}
+  constructor(private http:HttpService,@Logger('AppService') private logger: LoggerSrv) {}
   getHello(): string {
     return 'Hello World!';
   }
