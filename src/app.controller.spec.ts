884c8f840a12ebad305dbb41c3052a8eb3b293fb
Ajay <ajay.ctc@gmail.com>
2021-02-06 16:55:39 +0530

    Add files via upload
    modified

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,20 +1,40 @@
+import { HttpService } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let appController: AppController;
+  let appService: AppService;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            getHello: jest.fn().mockReturnValue('Hello World!'),
+          },
+        },
+        {
+          provide: HttpService,
+          useValue: {
+          },
+        },
+      ],
     }).compile();
+
+    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('getHello', () => {
+  describe('root', () => {
+    it('should return "Hello, World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
     it('should return "Hello World!"', () => {
-      const appController = app.get<AppController>(AppController);
+      appService.getHello = jest.fn().mockReturnValueOnce('Hello World!');
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
